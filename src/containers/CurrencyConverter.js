fd2b8473697b88898c0428e4fabe96f4e88513bd
Anton Tsivarev <a.tsivarev@corp.vk.com>
2018-07-09 09:51:13 +0300

    Fix iOS's selector: use Div instead FormLayout

--- src/containers/CurrencyConverter.js
+++ src/containers/CurrencyConverter.js
@@ -117,12 +117,12 @@ class CurrencyConverter extends Component {
 
     render() {
         return (
-            <UI.FormLayout>
+            <UI.Div>
                 <div style={{color: UI.colors.captionGray}}>
                     Курс
                     1 {this.getCurrencySymbol(this.state.sourceCurrencyCode)} = {this.getPrettyAmount(this.getCurrencyRate(this.state.sourceCurrencyCode, this.state.targetCurrencyCode))} {this.getCurrencySymbol(this.state.targetCurrencyCode)}
                 </div>
-                <div style={{display: "flex"}}>
+                <div style={{display: "flex", margin: "10px 0px"}}>
                     <div style={{flexGrow: 4, marginRight: 10}}>
                         <UI.Input type="number" placeholder="0" pattern="\d*"
                                   value={this.state.amount ? this.state.amount.toLocaleString() : ''} alignment="center"
@@ -136,7 +136,7 @@ class CurrencyConverter extends Component {
                         </UI.Select>
                     </div>
                 </div>
-                <div style={{display: "flex"}}>
+                <div style={{display: "flex", margin: "10px 0px"}}>
                     <div style={{flexGrow: 4, marginRight: 10}}>
                         <UI.Input type="text" alignment="center" disabled
                                   value={this.getPrettyAmount(this.state.convertedAmount)}/>
@@ -149,7 +149,7 @@ class CurrencyConverter extends Component {
                         </UI.Select>
                     </div>
                 </div>
-            </UI.FormLayout>);
+            </UI.Div>);
     }
 }
 
